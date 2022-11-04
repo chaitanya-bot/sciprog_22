ea3aa2a88398c17441d2b9d1d6b175b173c8187f
Chaitanya <chaitanyam700@gmail.com>
2022-11-04 18:03:23 +0000

    Changes in dynamicalloc c file and readme text file

--- practical7/dynamicalloc.c
+++ practical7/dynamicalloc.c
@@ -4,7 +4,7 @@
 int *allocatearray(int x)
 {
   int *p;
-  p = (int *)malloc(x * sizeof(int));
+  p = (int *)malloc(x * sizeof(int));   //Allocating memory using malloc
   return p;
 }
 
@@ -13,7 +13,7 @@ void fillwithones(int *p, int n)
    int i;
    for(i=0;i<n;i++)
    {
-       p[i] = 1;
+       p[i] = 1;           //Filling the array with ones using loop
    }
    
 }
@@ -23,21 +23,21 @@ void printarray(int *p,int n)
     int i;
     for(i=0;i<n;i++)
     {
-        printf("%d, ",p[i]);
+        printf("%d, ",p[i]);        //Printing the contents of array using for loop
     }
 }
 
 void deallocate(int *p)
 {
-    free(p);
+    free(p);                     //Freeing memory allocated to array through malloc
 }
 
 int main()
 {
     int n,*a;
-    printf("Enter the size of array ");
+    printf("Enter the size of array ");    //Asking the user to enter size of array
     scanf("%d",&n);
-    a = allocatearray(n);
+    a = allocatearray(n);    //Calling all 4 functions one after the other
     fillwithones(a,n);
     printarray(a,n);
     deallocate(a);
---
+++ practical7/readme7.txt
@@ -0,0 +1,8 @@
+In this weeks practical, there are 2 programs.
+
+The 1st program is find to find e^1 using taylor series. Here we are initiializing a dynamic array using malloc. Then to find the value, we need to use both factorial and power. 
+The factorial is found out using a for loop and power is taken from the math library. Finally we are printing the difference between e(1) theoretical value and the calue we got from
+taylor's series from 1st to 15th polynomial. The difference should reduce as the number of polynomial increases.
+
+The 2nd program has various functions. The 1st one is to allocate memory dynamically to an array and returing it. The 2nd function is to fill the array with all ones. The 3rd function is to 
+print the contents of the dynamic array. The 4th function is free memory of the array.
